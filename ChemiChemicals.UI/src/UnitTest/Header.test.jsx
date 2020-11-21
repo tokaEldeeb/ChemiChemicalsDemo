59b67a3c6cb90877c5fa2c09babe30abb7898a98
TOKA-ELDEEB\tokae <tokaeldeeb94@gmail.com>
2020-11-22 00:50:30 +0200

    remove extra code

--- ChemiChemicals.UI/src/UnitTest/Header.test.jsx
+++ ChemiChemicals.UI/src/UnitTest/Header.test.jsx
@@ -7,25 +7,6 @@ import Product from "../Services/Product";
 
 jest.mock("../Services/Product");
 
-const server = setupServer(
-    rest.get('/getRecentlyChangedProducts', (req, res, ctx) => {
-        return res(ctx.json([
-            {
-                id: "56432a9a-02c4-43eb-88e9-e5e4ce536201",
-                productName: 'product',
-                supplierName: "supplier",
-                url:"http://www.africau.edu/images/default/sample.pdf",
-                binaryContent: "",
-                isChanged: false,
-                insertDate: "11-10-2020"
-            }]))
-    })
-);
-
-beforeAll(() => server.listen());
-afterEach(() => server.resetHandlers());
-afterAll(() => server.close());
-
 test('renders header', () => {
     render(<Header />);
     var headerTitle = screen.getByText("Chemi Chemicals Interface");
